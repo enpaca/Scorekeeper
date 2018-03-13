7e2af7c6b9e15e0273b33687ff35c26007d3b851
En <en_hp@hotmail.com>
2018-03-13 15:34:03 +0100

    Push all the project files, because it didn't work while creating the repository

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.scorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //global fields:
+    int goalsForTeamA = 0;
+    int goalsForTeamB = 0;
+    int shotsOnTargetForTeamA = 0;
+    int shotsOnTargetForTeamB = 0;
+    int percentageForTeamA = 0;
+    int percentageForTeamB = 0;
+    int foulsForTeamA = 0;
+    int foulsForTeamB = 0;
+
+
+    //methods to update TextViews
+    public void updateGoalsTeamA() {
+        TextView goalsTeamA = (TextView) findViewById(R.id.goals_TeamA);
+        goalsTeamA.setText("1");
+    }
+
+
+    //methods to handle events(event handlers):
+    public void addOneShotOnTargetForTeamA(View view) {
+    }
+
+    public void addOneShotOnTargetForTeamB(View view) {
+    }
+
+    public void resetForTheNextGame(View view) {
+    }
+
+    public void addOneGoalForTeamA(View view) {
+    }
+
+    public void addOneGoalForTeamB(View view) {
+    }
+
+    public void addPercentageTeamA(View view) {
+    }
+
+    public void addPercentageTeamB(View view) {
+    }
+
+    public void addOneFoulForTeamA(View view) {
+    }
+
+    public void addOneFoulForTeamB(View view) {
     }
 }
